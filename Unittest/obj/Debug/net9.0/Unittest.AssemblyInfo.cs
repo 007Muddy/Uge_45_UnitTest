d1c1dd4ea97fa3e5fcaa9ffece8a5efc0f1f5f29
007Muddy <101377307+007Muddy@users.noreply.github.com>
2025-11-03 14:54:29 +0100

    zxc
    zxc

--- Unittest/obj/Debug/net9.0/Unittest.AssemblyInfo.cs
+++ Unittest/obj/Debug/net9.0/Unittest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unittest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8b90bbd910491eb6958f2e80e799a54e1a63faf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3ed91a1c6a6644a6483869f155962b38e40e9ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unittest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unittest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
