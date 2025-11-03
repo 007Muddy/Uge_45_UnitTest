307fa5bbb76092447fed872ad134bb49156ac787
007Muddy <101377307+007Muddy@users.noreply.github.com>
2025-11-03 13:02:07 +0100

    test
    test

---
+++ Unittest/Test-Firstname-domæneprimitivet.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using ValidateTheNameModelBinding.Models;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Firstname_ValidName_ShouldSetValue()
+        {
+            // Arrange
+            string validName = "Anne-Marie";
+
+            // Act
+            var firstname = new Firstname(validName);
+
+            // Assert
+            Assert.Equal(validName, firstname.Value);
+        }
+
+        [Fact]
+        public void Lastname_ValidName_ShouldSetValue()
+        {
+            // Arrange
+            string validName = "O'Connor";
+
+            // Act
+            var lastname = new Lastname(validName);
+
+            // Assert
+            Assert.Equal(validName, lastname.Value);
+        }
+
+        [Theory]
+        [InlineData("")]           // empty string
+        [InlineData(" ")]          // just space
+        [InlineData("123")]        // numbers
+        [InlineData("John!")]      // special char
+        public void Firstname_InvalidName_ShouldThrow(string invalidName)
+        {
+            Assert.Throws<ArgumentException>(() => new Firstname(invalidName));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("123")]
+        [InlineData("Doe@")]
+        public void Lastname_InvalidName_ShouldThrow(string invalidName)
+        {
+            Assert.Throws<ArgumentException>(() => new Lastname(invalidName));
+        }
+    }
+}
